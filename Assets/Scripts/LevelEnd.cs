1ea7f26ca110a06a563779b55a66aa86b7c45b26
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2024-01-28 05:43:02 +0000

    End Fade

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -1,17 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelEnd : MonoBehaviour
 {
     public LevelController levelController;
+
+    private bool fadeIn;
+    private bool fadeOut;
+
+    private CanvasGroup fadeCanvas;
+    public float fadeSpeed;
+    public float fadeDuration;
+    public float endWaitDuration;
+
     void Start()
     {
         levelController = GameObject.FindObjectOfType<LevelController>();
+        fadeCanvas = GameObject.Find("Fade Canvas").GetComponent<CanvasGroup>();
+        fadeIn = false;
+        fadeOut= false;
     }
     void OnTriggerEnter(Collider other)
     {
-        levelController.NextLevel();
+
+        StartCoroutine(EndWait());
     }
+
+    private void Update()
+    {
+        if (fadeIn)
+        {
+            if (fadeCanvas.alpha < 1)
+            {
+                fadeCanvas.alpha += fadeSpeed * Time.deltaTime;
+            }
+            else if (fadeCanvas.alpha >= 1)
+            {
+                StartCoroutine(FadeWait());
+            }
+        }
+
+        if (fadeOut)
+        {
+            if (fadeCanvas.alpha >= 0)
+            {
+                fadeCanvas.alpha -= fadeSpeed * Time.deltaTime;
+
+                if (fadeCanvas.alpha == 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
+    }
+
+    private IEnumerator EndWait()
+    {
+        yield return new WaitForSeconds(endWaitDuration);
+        fadeIn = true;
+        levelController.NextLevel();
+        StopCoroutine(EndWait());
+    }
+
+    private IEnumerator FadeWait()
+    {
+        fadeIn = false;
+        yield return new WaitForSeconds(fadeDuration);
+        fadeOut = true;
+        StopCoroutine(FadeWait());
+    }
+
+
 
 }
